37358256f25930299d6d3706eb9fa7d40abfeb37
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-01 23:23:11 +0200

    ajout de old

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -6,35 +6,35 @@
             <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
 
                 <h1 id="form_content"> Contact </h1>
-                 <form id="form" action="#" method="post">
+                 <form id="form" action="{{ route('contact') }}" method="post">
 
-                   <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                   {{ csrf_field() }}
 
                     <div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
                         <label for="name" class="control-label">Nom*</label>
-                        <input type="text" class="form-control" id="title" name="name" placeholder="Entrez votre nom">
+                        <input type="text" class="form-control" id="title" name="name" value="{{ old('name') }}" placeholder="Entrez votre nom">
                         <span class="label label-danger">{{ $errors->first('name') }}</span>
                     </div>
 
                      <div class="form-group {{ $errors->has('description') ? 'has-error' : ''}}">
                         <label for="telephone" class="control-label">Numéro de téléphone*</label>
-                        <input type="text" class="form-control" id="email" name="description" placeholder="Entrez votre numéro de téléphone">
+                        <input type="text" class="form-control" id="email" name="telephone" value="{{ old('telephone') }}" placeholder="Entrez votre numéro de téléphone">
                         <span class="label label-danger">{{ $errors->first('telephone') }}</span>
                      </div>
 
                      <div class="form-group {{ $errors->has('description') ? 'has-error' : ''}}">
                         <label for="email" class="control-label">Adresse email*</label>
-                        <input type="text" class="form-control" id="email" name="description" placeholder="Entrez votre nom">
+                        <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }} "placeholder="Entrez votre nom">
                         <span class="label label-danger">{{ $errors->first('email') }}</span>
-                      </div>
+                     </div>
 
                      <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
                         <label for="message" class="control-label">Message </label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+                        <textarea class="form-control" name="message" rows="6"></textarea>
                         <span class="label label-danger">{{ $errors->First('message') }}</span>
-                      </div>
+                     </div>
 
-                      <button id="upload-button" type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
+                     <button id="upload-button" type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
                 </form>
 
             </div>
