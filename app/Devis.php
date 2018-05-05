97c57f5b1a8f451bf0b944cd49887318ffbedee4
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-05 17:32:21 +0200

    ajout de champs en fillable

--- app/Devis.php
+++ app/Devis.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Devis extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code_postal', 'type_appartement', 'nombre_pieces', 'prestations', 'message', 'surface'
+    ];
+
     protected $table = 'devis';
 }
