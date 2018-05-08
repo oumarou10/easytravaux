5199aca808cb44d6da58204b25fe752a2fef1e0d
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-08 02:29:05 +0200

    image header disposée avec bootstrap

---
+++ resources/views/layout/img.blade.php
@@ -0,0 +1,10 @@
+<div id="header_img">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-1">
+            <img width="44%" src="{{ asset('images/pic_devis.png') }}  ">
+        </div>
+        <div class="col-md-5 col-md-offset-2">
+            <img width="60%" src="{{ asset('images/pic_title.png') }}  ">
+        </div>
+    </div>
+</div>
