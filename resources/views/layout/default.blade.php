c95e56b28c91ba1afb485257af00e497e060866c
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-09 00:50:52 +0200

    Ajout font spéciale

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <title>{{ page_title($title ?? '') }}</title>
+    <link href='https://fonts.googleapis.com/css?family=Rubik' rel='stylesheet'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 </head>
