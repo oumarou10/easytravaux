72b72dfc9f87a034a2233022f9a8955df125b2ea
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-08 02:31:41 +0200

    Ajustement template form

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -1,11 +1,19 @@
 @extends('layout.default', ['title' => 'Contact'])
 
 @section('content')
-    <div id="container_contact" class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+    <div class="container">
+        <div class="row" style="background-color: ">
 
-                <h1 id="form_content"> Contact </h1>
+            <h1> Contact </h1>
+
+            <hr class="style3">
+
+            <div class="col-md-6 col-md-offset-1">
+
+                <h3> Nous écrire </h3>
+
+                <p>Une question ? Appelez-moi au : 07 58 06 90 48 ou envoyez-nous un message ci-dessous.</p>
+
                  <form id="form" action="{{ route('contact') }}" method="post">
 
                    {{ csrf_field() }}
@@ -17,8 +25,8 @@
                     </div>
 
                      <div class="form-group {{ $errors->has('description') ? 'has-error' : ''}}">
-                        <label for="telephone" class="control-label">Numéro de téléphone*</label>
-                        <input type="text" class="form-control" id="email" name="telephone" value="{{ old('telephone') }}" placeholder="Entrez votre numéro de téléphone">
+                        <label for="telephone" class="control-label">Numéro de téléphone</label>#343837
+                        <input type="text" class="form-control" id="email" name="telephone" value="{{ old('telephone')}}" placeholder="Entrez votre numéro de téléphone">
                         <span class="label label-danger">{{ $errors->first('telephone') }}</span>
                      </div>
 
@@ -30,12 +38,26 @@
 
                      <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
                         <label for="message" class="control-label">Message </label>
-                        <textarea class="form-control" name="message" rows="6"></textarea>
+                        <textarea class="form-control" name="message" rows="4"></textarea>
                         <span class="label label-danger">{{ $errors->First('message') }}</span>
                      </div>
 
-                     <button id="upload-button" type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
+                     <h5 class="lowercased text text-right">*champs obligatoires</h5>
+
+                     <button id="upload-button" type="submit" class="btn btn-primary btn-lg btn-block">Envoyer</button>
                 </form>
+
+            </div>
+
+            <div class="adresse col-md-4 col-md-offset-1">
+
+               <h3> Adresse </h3>
+
+                <p> 112, avenue lénine <br> 93380 Pierrefitte-sur-Seine</p>
+
+                <p><a href="mailto:amadicoulibaly67@gmail.com">amadicoulibaly67@gmail.com</a><br>Tél : 07 58 06 90 48
+                </p>
+
 
             </div>
         </div>
--- resources/views/devis/form.blade.php
+++ resources/views/devis/form.blade.php
@@ -1,51 +1,61 @@
-@extends('layout.default', ['title' => 'Devis'])
+@extends('layout.default', ['title' => 'Contact'])
 
 @section('content')
     <div class="container">
+        <div class="row" style="background-color: ">
 
-        <h1 style="text-align: center"> Devis</h1>
+            <h1> Devis </h1>
+            <hr class="style3">
 
-        <form id="Form" action="{{ route('devis') }}" method="post">
+            <div class="col-md-6 col-md-offset-1">
+                <h3> Besoin d'un devis </h3>
+                <p>Une question ? Appelez-moi au : 07 58 06 90 48 ou envoyez-nous un message ci-dessous.</p>
 
-           {{ csrf_field() }}&
+                <form id="Form" action="{{ route('devis') }}" method="post">
 
-           <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
-            <label for="type-appartement" class="control-label">Type d'habitation: </label> <br>
-            <input type='radio' name='type-appartement' value='Option One'> Appartement
-            <input type='radio' name='type-appartement' value='Option Two'> Maison individuelle <br>
-            <span class="label label-danger">{{ $errors->first('type-appartement') }}</span>
-          </div>
+                    {{ csrf_field() }}
 
-          <div class="form-group {{ $errors->has('code_postal') ? 'has-error' : ''}}">
-            <label for="code_postal" class="control-label">Code postal:</label>
-            <input type="text" class="form-control" id="code_postal" name="code_postal" placeholder="Entrez votre code postal">
-            <span class="label label-danger">{{ $errors->first('code_postal') }}</span>
-          </div>
+                    <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
+                        <label for="type-appartement" class="control-label">Type d'habitation: </label> <br>
+                        <input type='radio' class="form-control" name='type-appartement' value='Option One'> Appartement
+                        <input type='radio' class="form-control" name='type-appartement' value='Option Two'> Maison individuelle <br>
+                        <span class="label label-danger">{{ $errors->first('type-appartement') }}</span>
+                    </div>
 
-          <div class="form-group {{ $errors->has('prestations') ? 'has-error' : ''}}">
-            <label for="name" class="control-label">Type de travaux: </label> <br>
-              <input type='checkbox' name='prestations[]' value='Option One'> Fenetres
-              <input type='checkbox' name='prestations[]' value='Option two'> Electricité
-              <input type='checkbox' name="prestations[]" value='Option three'> Rénovation salle de bain<br>
-              <input type='checkbox' name='prestations[]' value='Option four'> Rénovation cuisine <br>
-              <input type='checkbox' name='prestations[]' value='Option five'> Peinture <br>
-              <input type="checkbox" name='prestations[]' value='Option six'> Toutes types de rénovations sol - murs <br>
-              <input type="checkbox" name='prestations[]' value='Option seven'> Autres
-            <span class="label label-danger">{{ $errors->First('prestations') }}</span>
-          </div>
+                    <div class="form-group {{ $errors->has('code_postal') ? 'has-error' : ''}}">
+                        <label for="code_postal" class="control-label">Code postal:</label>
+                        <input type="text" class="form-control" id="code_postal" name="code_postal" placeholder="Entrez votre code postal">
+                        <span class="label label-danger">{{ $errors->first('code_postal') }}</span>
+                    </div>
 
-          <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
-            <label for="surface" class="control-label">Surface en m2: </label> <br>
-            <input type='number' min="5" name='surface' value='Option One'>
-            <span class="label label-danger">{{ $errors->First('name') }}</span>
-          </div>
+                    <div class="form-group {{ $errors->has('prestations') ? 'has-error' : ''}}">
+                        <label for="name" class="control-label">Type de travaux: </label> <br>
+                            <input type='checkbox' name='prestations[]' value='Option One'> Fenetres
+                            <input type='checkbox' name='prestations[]' value='Option two'> Electricité
+                            <input type='checkbox' name="prestations[]" value='Option three'> Rénovation salle de bain<br>
+                            <input type='checkbox' name='prestations[]' value='Option four'> Rénovation cuisine <br>
+                            <input type='checkbox' name='prestations[]' value='Option five'> Peinture <br>
+                            <input type="checkbox" name='prestations[]' value='Option six'> Toutes types de rénovations sol - murs <br>
+                            <input type="checkbox" name='prestations[]' value='Option seven'> Autres
+                        <span class="label label-danger">{{ $errors->First('prestations') }}</span>
+                    </div>
 
-            <h5 class="lowercased text text-right">*champs obligatoires</h5>
+                    <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
+                        <label for="surface" class="control-label">Surface en m2: </label> <br>
+                        <input type='number' min="5" class="form-control" name='surface' value='Option One'>
+                        <span class="label label-danger">{{ $errors->First('name') }}</span>
+                    </div>
+                    <h5 class="lowercased text text-right">*champs obligatoires</h5>
 
-          <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
-        </form>
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">Envoyez</button>
+                </form>
 
-        <div id="results"></div>
+            </div>
+
+            <div class="adresse col-md-4 col-md-offset-1">
+                <img width="100%" src="{{ asset('images/pic_peinture.jpg') }}  ">
+            </div>
+        </div>
 
     </div>
 
