585a1eccaf6404c470d491d62d9b33d98ef66cbe
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-08 20:12:42 +0200

    ajout wrapper / creation footer position absolute

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
-<html lang="fr">
-  <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <title>{{ page_title($title ?? '') }}</title>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-      <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-  </head>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <title>{{ page_title($title ?? '') }}</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+</head>
 
-  <body>
-      @include('layout.img')
-      @include('layout._nav')
-      @include('flashy::message')
-      @yield('content')
-      <script src="//code.jquery.com/jquery.js"></script>
-      @include('layout.footer')
-  </body>
+<body>
+<div class="global">
+    @include('layout.img')
+    @include('layout._nav')
+    @yield('content')
+    @include('flashy::message')
+    @include('layout.footer')
+</div>
+</body>
 </html>
