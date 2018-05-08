114ebea2537a568be47c87442944531182dd0621
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-08 02:27:31 +0200

    Suppression title sur navbar

--- resources/views/layout/_nav.blade.php
+++ resources/views/layout/_nav.blade.php
@@ -7,11 +7,10 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-          <a class="navbar-brand" style="color: white" href="{{ route('home') }}">Travaux facile 93</a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="{{set_active_route('home')}}"><a href="{{ route('home') }}" style="color: white">Home</a></li>
+                <li class="{{set_active_route('home')}}"><a href="{{ route('home') }}" style="color: white">Accueil</a></li>
                 <li class="{{set_active_route('devis')}}"><a href="{{ route('devis') }}" style="color: white">Devis</a></li>
                 <li class="{{set_active_route('about')}}"><a href="{{ route('about') }}" style="color: white">À Propos</a></li>
                 <li class="{{set_active_route('contact')}}"><a href="{{ route('contact') }}" style="color: white">Contact</a></li>
