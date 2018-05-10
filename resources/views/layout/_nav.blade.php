09dd0a1210e7712ad98f7da763996a469d2d261c
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-10 14:21:24 +0200

    modification de la navbar

--- resources/views/layout/_nav.blade.php
+++ resources/views/layout/_nav.blade.php
@@ -1,19 +1,18 @@
-<nav class="navbar navbar-default navbar-static-top" style="">
+<nav class="navbar navbar-default">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
             </button>
         </div>
-        <div class="navbar-collapse collapse">
+        <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
-                <li class="{{set_active_route('home')}}"><a href="{{ route('home') }}" style="color: white">Accueil</a></li>
-                <li class="{{set_active_route('devis')}}"><a href="{{ route('devis') }}" style="color: white">Devis</a></li>
-                <li class="{{set_active_route('about')}}"><a href="{{ route('about') }}" style="color: white">À Propos</a></li>
-                <li class="{{set_active_route('contact')}}"><a href="{{ route('contact') }}" style="color: white">Contact</a></li>
+                <li class="{{set_active_route('home')}}"><a href="{{ route('home') }}">Accueil</a></li>
+                <li class="{{set_active_route('devis')}}"><a href="{{ route('devis') }}">Devis</a></li>
+                <li class="{{set_active_route('about')}}"><a href="{{ route('about') }}">À Propos</a></li>
+                <li class="{{set_active_route('contact')}}"><a href="{{ route('contact') }}">Contact</a></li>
             </ul>
         </div>
     </div>
