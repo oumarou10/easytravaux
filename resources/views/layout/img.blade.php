7f68371e287f78fdf13f120fef8e8e4d246dbe3d
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-12 03:39:52 +0200

    responsive sur image

--- resources/views/layout/img.blade.php
+++ resources/views/layout/img.blade.php
@@ -1,9 +1,9 @@
 <div id="header_img">
     <div class="row">
-        <div class="col-md-4 col-md-offset-1">
+        <div class=" col-xs-6 col-sm-6 col-md-4 col-md-offset-1">
             <img width="44%" src="{{ asset('images/pic_devis.png') }}  ">
         </div>
-        <div class="col-md-5 col-md-offset-2">
+        <div class="col-xs-6 col-sm-6 col-md-5 col-md-offset-2 ">
             <img width="60%" src="{{ asset('images/pic_title.png') }}  ">
         </div>
     </div>
