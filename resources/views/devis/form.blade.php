db8ddd593b7ffd80042bd09f9bd9f3270491ecfb
oumarucho <sissoko.oumarou1991@mail.com>
2018-05-12 03:21:38 +0200

    utilisation checkbox via tableau / ajout message

--- resources/views/devis/form.blade.php
+++ resources/views/devis/form.blade.php
@@ -24,20 +24,21 @@
                     {{ csrf_field() }}
 
                     <div class="form-group {{ $errors->has('type_appartement') ? 'has-error' : ''}}">
-                        <label for="surface" class="control-label">Type d'appartement: </label> <br>
-                        <label class="radio-inline"><input type="radio" name="type_appartement">Maison</label>
-                        <label class="radio-inline"><input type="radio" name="type_appartement">Appartement</label>
+                        <label for="type_appartement" class="control-label">Pièce à travailler* </label> <br>
+                        @foreach($items as $item)
+                            <label class="radio-inline"><input type="radio" name="type_appartement" value="{{ $item }}"> {{ $item }}</label>
+                        @endforeach
                         <span class="label label-danger">{{ $errors->first('type_appartement') }}</span>
                     </div>
 
                     <div class="form-group {{ $errors->has('code_postal') ? 'has-error' : ''}}">
-                        <label for="code_postal" class="control-label">Code postal:</label>
+                        <label for="code_postal" class="control-label">Code postal*</label>
                         <input type="text" class="form-control" id="code_postal" name="code_postal" placeholder="Entrez votre code postal">
                         <span class="label label-danger">{{ $errors->first('code_postal') }}</span>
                     </div>
 
                     <div class="form-group {{ $errors->has('prestations') ? 'has-error' : ''}}">
-                        <label for="surface" class="control-label">Type de travaux:</label> <br>
+                        <label for="prestations" class="control-label">Type de travaux*</label> <br>
                         <select class="control-label" name="prestations">
                              @foreach($prestations as $prestation)
                                 <option value="{{$prestation}}">{{$prestation}}</option>
@@ -47,10 +48,16 @@
                     </div>
 
                     <div class="form-group {{ $errors->has('surface') ? 'has-error' : ''}}">
-                        <label for="surface" class="control-label">Surface en m2: </label> <br>
+                        <label for="surface" class="control-label">Surface en m2*</label> <br>
                         <input type='number' min="5" class="form-control" name='surface' value='Option One'>
                         <span class="label label-danger">{{ $errors->first('surface') }}</span>
                     </div>
+
+                    <div class="form-group {{ $errors->has('message') ? 'has-error' : ''}}">
+                        <label for="message" class="control-label">N'hésitez pas à apporter des précisions </label>
+                        <textarea class="form-control" name="message" rows="4"></textarea>
+                        <span class="label label-danger">{{ $errors->first('message') }}</span>
+                     </div>
 
                     <div class="button">
                         <h5 class="lowercased text text-right">*champs obligatoires</h5>
@@ -61,7 +68,7 @@
             </div>
 
             <div class="adresse col-md-4">
-                <img width="100%" src="{{ asset('images/pic_peinture.jpg') }}  ">
+                <img width="100%" src="{{ asset('images/pic_peinture.jpg') }} ">
             </div>
         </div>
 
